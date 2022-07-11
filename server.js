a058c486395c0299c2dbe7096215e85e2cd5847f
Midas <goncalomidoes@tecnico.ulisboa.pt>
2022-07-11 11:53:19 +0100

    Updated link

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const apiFenix = require('./api_Fenix');
+const apiFenix = require('./api_fenix');
 const express = require('express')
 const app = express()
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://masters.goncalo-midoes.engineer/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
